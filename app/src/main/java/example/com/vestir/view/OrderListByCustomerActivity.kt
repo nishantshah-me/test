fa84b89326d36feae34c58c490aefcc1641b040e
mohan <mohan.singh@neosofttech.com>
2019-06-02 14:39:27 +0530

    Appcompat textview added in order screen

--- app/src/main/java/example/com/vestir/view/OrderListByCustomerActivity.kt
+++ app/src/main/java/example/com/vestir/view/OrderListByCustomerActivity.kt
@@ -4,6 +4,7 @@ import android.arch.lifecycle.Observer
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import example.com.vestir.IS_FOR_UPDATE
 import example.com.vestir.ORDER_DATA
@@ -17,11 +18,24 @@ class OrderListByCustomerActivity : AppCompatActivity(), OrderListAdapter.OnOrde
 
     private lateinit var adapter: OrderListAdapter
     private lateinit var database: AppDatabase
+    private var orderNames: MutableList<String> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_order_list_by_customer)
         database = AppDatabase.getInstance(this)
+
+        database.orderDao().getOrderList().observe(this, Observer {
+            val size = it!!.size - 1
+            for (i in 0..size) {
+                orderNames.add(i, it.get(i).name)
+            }
+
+            val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, orderNames)
+            etName.setAdapter(adapter)
+
+        })
+
 
         setTextToButton(getString(R.string.new_order))
         adapter = OrderListAdapter(this, this, null)
