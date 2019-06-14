5f1881a6fe5747e7217770795b87551a17a3c7bb
Nishant <nishant.shah@wwindia.com>
2019-06-14 19:00:49 +0530

    order screen changes

--- app/src/main/java/example/com/vestir/view/clientorder/CreateOrderActivity.kt
+++ app/src/main/java/example/com/vestir/view/clientorder/CreateOrderActivity.kt
@@ -2,15 +2,15 @@ package example.com.vestir.view.clientorder
 
 import android.app.Activity
 import android.app.DatePickerDialog
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.text.TextUtils
-import android.view.View
-import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import example.com.vestir.*
 import example.com.vestir.database.AppDatabase
 import example.com.vestir.database.entity.ClientOrder
+import example.com.vestir.view.client.MeasurementActivity
 import kotlinx.android.synthetic.main.activity_create_order.*
 import kotlinx.android.synthetic.main.layout_toolbar.*
 import java.text.SimpleDateFormat
@@ -21,6 +21,7 @@ class CreateOrderActivity : AppCompatActivity() {
     private var isUpdate: Boolean = false
     private var updateOrderId: Long = 0
     private var selectedClientId: Long = 0
+    private var isEdited: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,51 +29,76 @@ class CreateOrderActivity : AppCompatActivity() {
 
         database = AppDatabase.getInstance(this)
         selectedClientId = intent.getLongExtra(SELECT_CLIENT_ID, 0)
-        if(intent.getBooleanExtra(IS_FOR_UPDATE, false) && intent.hasExtra(ORDER_DATA)){
+        if (intent.getBooleanExtra(IS_FOR_UPDATE, false) && intent.hasExtra(ORDER_DATA)) {
             isUpdate = true
             btnSubmit.text = getString(R.string.str_update)
             val order = intent.getSerializableExtra(ORDER_DATA) as ClientOrder
             showData(order)
         } else {
-            etName.setText(intent.getStringExtra(SELECTED_CLIENT_NAME))
-            etName.isEnabled = false
+            spnStatus.isEnabled = false
+            etName.text = intent.getStringExtra(SELECTED_CLIENT_NAME)
+            val sdf = SimpleDateFormat(ORDER_DATE_FORMAT)
+            etorder.text = sdf.format(Calendar.getInstance().time)
         }
 
         btnSubmit.setOnClickListener { addOrder() }
         img_back.setOnClickListener { onBackPressed() }
-        etorder.setOnClickListener {
-            showDatePicker("order")
-        }
+        txt_measurements.setOnClickListener { onMeasurementsClick() }
         etTrial.setOnClickListener {
-            showDatePicker("trial")
+            if (!TextUtils.isEmpty(etorder.text.toString())) {
+                try {
+                    val sdf = SimpleDateFormat(ORDER_DATE_FORMAT)
+                    val minDate = sdf.parse(etorder.text.toString().trim())
+                    showDatePicker("trial", minDate.time)
+                } catch (e: Exception) {
+
+                }
+            }
         }
         etDelivery.setOnClickListener {
-            showDatePicker("delivery")
+            if (!TextUtils.isEmpty(etTrial.text.toString())) {
+                try {
+                    val sdf = SimpleDateFormat(ORDER_DATE_FORMAT)
+                    val minDate = sdf.parse(etTrial.text.toString().trim())
+                    showDatePicker("delivery", minDate.time)
+                } catch (e: Exception) {
+
+                }
+            }
         }
     }
 
-    private fun showDatePicker(date: String){
+    private fun showDatePicker(date: String, minDate: Long) {
         val calendar = Calendar.getInstance()
-        DatePickerDialog(this@CreateOrderActivity,
+        val datePickerDialog = DatePickerDialog(this@CreateOrderActivity,
                 DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
                     val cal = Calendar.getInstance()
                     cal.set(Calendar.YEAR, year)
                     cal.set(Calendar.MONTH, month)
                     cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
 
-                    val sdf = SimpleDateFormat(ORDER_DATE_FORMAT)
-                    when(date){
-                        "order" -> etorder.text = sdf.format(cal.time)
-                        "trial" -> etTrial.text = sdf.format(cal.time)
-                        "delivery" -> etDelivery.text = sdf.format(cal.time)
+                    try {
+                        val sdf = SimpleDateFormat(ORDER_DATE_FORMAT)
+                        when (date) {
+                            "order" -> etorder.text = sdf.format(cal.time)
+                            "trial" -> etTrial.text = sdf.format(cal.time)
+                            "delivery" -> etDelivery.text = sdf.format(cal.time)
+                        }
+                    } catch (e: Exception) {
+
                     }
                 }, calendar.get(Calendar.YEAR),
                 calendar.get(Calendar.MONTH),
-                        calendar.get(Calendar.DAY_OF_MONTH)).show()
+                calendar.get(Calendar.DAY_OF_MONTH))
+        datePickerDialog.datePicker.minDate = minDate
+        datePickerDialog.show()
     }
 
-    private fun addOrder(){
-        if(validateInpute()){
+    private fun addOrder() {
+        if (validateInput()) {
+            var quoteValue = etQuote.text.toString()
+            if(TextUtils.isEmpty(quoteValue))
+                quoteValue = "0"
             val order = ClientOrder().apply {
                 clientId = selectedClientId
                 name = etName.text.toString()
@@ -82,25 +108,26 @@ class CreateOrderActivity : AppCompatActivity() {
                 order = etorder.text.toString()
                 trial = etTrial.text.toString()
                 delivery = etDelivery.text.toString()
-                quote = etQuote.text.toString().toLong()
+                quote = quoteValue.toLong()
             }
             var message = "Order added successfully"
-            if(isUpdate){
+            if (isUpdate) {
                 order.orderId = updateOrderId
                 database.orderDao().updateOrder(order)
                 message = "Order updated successfully"
             } else {
                 database.orderDao().addOrder(order)
             }
-            Toast.makeText(this@CreateOrderActivity, message , Toast.LENGTH_SHORT).show()
-            setResult(Activity.RESULT_OK)
-            finish()
+            isEdited = true
+            Toast.makeText(this@CreateOrderActivity, message, Toast.LENGTH_SHORT).show()
+            //setResult(Activity.RESULT_OK)
+            //finish()
         }
     }
 
     private fun showData(order: ClientOrder) {
         updateOrderId = order.orderId
-        etName.setText(order.name)
+        etName.text = order.name
         etName.isEnabled = false
         etStyle.setText(order.style)
         etDesc.setText(order.description)
@@ -110,42 +137,31 @@ class CreateOrderActivity : AppCompatActivity() {
         index = if (index < 0) 0 else index
         spnStatus.setSelection(index)
 
-        etorder.setText(order.order)
-        etTrial.setText(order.trial)
-        etDelivery.setText(order.delivery)
+        etorder.text = order.order
+        etTrial.text = order.trial
+        etDelivery.text = order.delivery
         etQuote.setText(order.quote.toString())
     }
 
-    private fun validateInpute() : Boolean{
-        if(TextUtils.isEmpty(etName.text.toString())){
-            etName.requestFocus()
-            etName.error = "Name is empty"
-            return false
-        } else if(TextUtils.isEmpty(etStyle.text.toString())){
+    private fun validateInput(): Boolean {
+        if (TextUtils.isEmpty(etStyle.text.toString())) {
             etStyle.requestFocus()
             etStyle.error = "Style is empty"
             return false
-        } else if(TextUtils.isEmpty(etDesc.text.toString())){
-            etDesc.requestFocus()
-            etDesc.error = "Description is empty"
-            return false
-        } else if(TextUtils.isEmpty(etorder.text.toString())){
-            etorder.requestFocus()
-            etorder.error = "Order is empty"
-            return false
-        } else if(TextUtils.isEmpty(etTrial.text.toString())){
-            etTrial.requestFocus()
-            etTrial.error = "Trial is empty"
-            return false
-        } else if(TextUtils.isEmpty(etDelivery.text.toString())){
-            etDelivery.requestFocus()
-            etDelivery.error = "Delivery is empty"
-            return false
-        } else if(TextUtils.isEmpty(etQuote.text.toString())){
-            etQuote.requestFocus()
-            etQuote.error = "Quote is empty"
-            return false
         }
         return true
+    }
+
+    private fun onMeasurementsClick(){
+        val intent = Intent(this@CreateOrderActivity, MeasurementActivity::class.java)
+        intent.putExtra("clientId", selectedClientId)
+        startActivity(intent)
+    }
+
+    override fun onBackPressed() {
+        if(isEdited){
+            setResult(Activity.RESULT_OK)
+        }
+        finish()
     }
 }
