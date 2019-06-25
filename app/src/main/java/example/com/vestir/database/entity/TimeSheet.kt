562989acf888912f7d3cd8623585e1a68e689aa1
Nishant shah <nishant.shah@wwindia.com>
2019-06-25 12:16:12 +0530

    timesheet logic

--- app/src/main/java/example/com/vestir/database/entity/TimeSheet.kt
+++ app/src/main/java/example/com/vestir/database/entity/TimeSheet.kt
@@ -1,5 +1,6 @@
 package example.com.vestir.database.entity
 
+import android.arch.persistence.room.ColumnInfo
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
 
@@ -11,6 +12,7 @@ class TimeSheet{
     var timeSheetDate : String = ""
     var staffName : String = ""
     var timeSheetHours : Int = 0
-    var clientName : String = ""
+    @ColumnInfo(name = "clientName_order")
+    var clientNameAndOrder : String = ""
     var description : String = ""
 }
--- app/src/main/java/example/com/vestir/view/timesheet/TimeSheetActivity.kt
+++ app/src/main/java/example/com/vestir/view/timesheet/TimeSheetActivity.kt
@@ -4,13 +4,17 @@ import android.app.DatePickerDialog
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.CardView
-import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.LinearLayout
 import example.com.vestir.ORDER_DATE_FORMAT
 import example.com.vestir.R
+import example.com.vestir.R.id.etDate
+import example.com.vestir.R.id.llTimeSheetRow
+import example.com.vestir.database.AppDatabase
+import example.com.vestir.database.dao.ClientDao
 import example.com.vestir.database.entity.TimeSheet
 import kotlinx.android.synthetic.main.activity_time_sheet.*
 import kotlinx.android.synthetic.main.layout_toolbar.*
@@ -20,14 +24,26 @@ import java.util.*
 
 
 class TimeSheetActivity : AppCompatActivity() {
-    private lateinit var view : View
+    private lateinit var view: View
     private var count = 0
-    private val timeSheetData : MutableList<TimeSheet> = ArrayList()
+    lateinit var clientDao: ClientDao
+    lateinit var adapter: ArrayAdapter<String>
+    var clientNames: MutableList<String>? = null
+    private val timeSheetData: MutableList<TimeSheet> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_time_sheet)
-        addTimeSheet()
+        clientDao = AppDatabase.getInstance(this).clientDao()
+        clientDao.getAllClient().observe(this, android.arch.lifecycle.Observer {
+            val size = it!!.size - 1
+            for (i in 0..size) {
+                clientNames?.add(i, it.get(i).name)
+            }
+            adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, clientNames)
+            addTimeSheet()
+
+        })
 
         img_back.setOnClickListener { onBackPressed() }
 
@@ -35,32 +51,39 @@ class TimeSheetActivity : AppCompatActivity() {
             addTimeSheet()
         }
 
-        etDate.setOnClickListener{
+        etDate.setOnClickListener {
 
-                try {
-                    val sdf = SimpleDateFormat(ORDER_DATE_FORMAT)
-
-                    showDatePicker()
-                } catch (e: Exception) {
+            try {
+                showDatePicker()
+            } catch (e: Exception) {
 
 
             }
         }
+
+        btnSubmit.setOnClickListener { saveTimeSheet() }
     }
 
-    fun addTimeSheet(){
+    private fun saveTimeSheet() {
+        for (i in 0 until llTimeSheetRow.childCount - 1) {
+
+        }
+    }
+
+    fun addTimeSheet() {
         val inflater = LayoutInflater.from(this)
-        view =  inflater.inflate(R.layout.row_time_sheet,null)
+        view = inflater.inflate(R.layout.row_time_sheet, null)
+
+
+        view.etClientName.setAdapter(adapter)
         view.imgDelCard.tag = count++
         val params = LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
         view.isClickable = true
 
         params.topMargin = 16
-        llTimeSheetRow.addView(view,params)
+        llTimeSheetRow.addView(view, params)
 
         view.imgDelCard.setOnClickListener {
-
-
             val parentView = ((it.parent as LinearLayout).parent as CardView).parent as LinearLayout
             val index = llTimeSheetRow.indexOfChild(parentView)
             llTimeSheetRow.removeViewAt(index)
